203060fcd4e36dad8e8ffef9d03a1e0b31349174
InvestNaira <toni@kidsinspiringnation.org>
2026-07-04 16:09:15 +0100

    TREASURE front page: product landing, SEO (sitemap, robots, OG, JSON-LD), live market components

---
+++ _astro/App.CB5xucrG.js
@@ -0,0 +1,1 @@
+import{A as a}from"./App.Cb6UoObz.js";import"./index.C41XeAxG.js";export{a as default};
--- config.example.js
+++
@@ -1,9 +0,0 @@
-window.INVESTNAIRA_CONFIG = {
-  SUPABASE_URL: '',
-  SUPABASE_ANON: '',
-  GHOST_URL: 'https://research.investnaira.com',
-  GHOST_KEY: '',
-  PAY_PREMIUM: '',
-  PAY_PROFESSIONAL: '',
-  PAY_BUSINESS: 'mailto:research@investnaira.com',
-};
